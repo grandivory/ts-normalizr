0721f9d558e7cb03d89a889a1050f8284bd4ffe7
Matthew Butt <grandivory@gmail.com>
2021-06-23 17:33:27 -0400

    Simplify the result type for objectValues

--- src/schemas/ObjectValues.d.ts
+++ src/schemas/ObjectValues.d.ts
@@ -1,24 +1,24 @@
-import { NormalizationOutput, NormalizationResultType, Schema, SchemaPropBuilder, ValidKey , PropBuilder } from "../types";
+import { NormalizationOutput, Schema, SchemaPropBuilder, ValidKey , PropBuilder } from "../types";
 import { EntitySchema } from "./Entity";
 
 export class ObjectValuesSchema<
   Input extends Record<ValidKey, any>,
   Name extends string,
   EntitiesOutput extends Record<string, Record<string, any>>
-> implements Schema<Input, Name, Record<string, NormalizationResultType>, EntitiesOutput> {
+> implements Schema<Input, Name, Record<string, string>, EntitiesOutput> {
   nameProp: Name;
   entitySchema: EntitySchema<any, any, Name, any, EntitiesOutput>;
 
   constructor(entitySchema: EntitySchema<any, any, Name, any, EntitiesOutput>)
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  normalizeWith(input: Input, parent: any): NormalizationOutput<Record<string, NormalizationResultType>, EntitiesOutput>
+  normalizeWith(input: Input, parent: any): NormalizationOutput<Record<string, string>, EntitiesOutput>
 }
 
 export class ObjectValuesBuilder<T extends string>
-  extends PropBuilder<T, Record<string, NormalizationResultType>> {}
+  extends PropBuilder<T, Record<string, string>> {}
 
-export class ObjectValuesSchemaBuilder<T extends string, S extends EntitySchema<any, any, T, any, any>> extends ObjectValuesBuilder<T> implements SchemaPropBuilder<T, Record<string, NormalizationResultType>, S> {
+export class ObjectValuesSchemaBuilder<T extends string, S extends EntitySchema<any, any, T, any, any>> extends ObjectValuesBuilder<T> implements SchemaPropBuilder<T, Record<string, string>, S> {
   schema: S;
 
   constructor(input: S)
